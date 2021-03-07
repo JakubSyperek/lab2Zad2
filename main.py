207e00ea34856b932d9fab4398c9cf3a1ff03f1c
JakubSyperek <162420@student.uwm.edu.pl>
2021-03-07 20:38:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+filmy = {'Brad Bird':'Ratatuj',
+         'Tadeusz Chmielewski':'Nie lubie poniedzialku',
+         'Olaf Lubaszenko':'Poranek kojota',
+         'Quentin Tarantino':'Pulp Fiction',
+         'Juliusz Machulski':'Kiler',
+         'Andrew Adamson':'Shrek 2',
+         'Marek Koterski':'Dzien swira',
+         'Jerzy Gruza':'Gulczas, a jak myslisz?',
+         'Georges Melies':'Podroz na ksiezyc',
+         'Olaf Lubaszenko':'Chlopaki nie placza'}
+
